78d357044091e958f9c4e370ba632af3a8714b3d
Emerson Santana <87781497+EmersonSantana1994@users.noreply.github.com>
2021-08-25 08:06:10 -0300

    Add files via upload
    Atividade de Spring BlogPessoal

---
+++ blogPessoal/controller/PostagemController.java
@@ -0,0 +1,27 @@
+package com.example.blogPessoal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.blogPessoal.model.Postagem;
+import com.example.blogPessoal.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin("*")
+public class PostagemController {
+
+	@Autowired
+	private PostagemRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> GetAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+}
