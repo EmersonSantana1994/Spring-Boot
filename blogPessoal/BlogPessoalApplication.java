790e89df33d79fcb9caf5f1ee96c2039dbf6ba71
Emerson Santana <87781497+EmersonSantana1994@users.noreply.github.com>
2021-08-25 22:33:07 -0300

    Add files via upload
    Atividade CRUD Back-end blog-pessoal

--- blogPessoal/BlogPessoalApplication.java
+++ blogPessoal/BlogPessoalApplication.java
@@ -3,6 +3,8 @@ package com.example.blogPessoal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//http://localhost:8080/postagens
+
 @SpringBootApplication
 public class BlogPessoalApplication {
 
